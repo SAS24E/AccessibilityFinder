7164f785ef7704289c8a1231a31f6c87847af5c0
Alex Secor <sas24e@fsu.edu>
2025-11-17 17:56:04 -0600

    Removed Username from login page.

--- application/views/login-dashboard.php
+++ application/views/login-dashboard.php
@@ -27,10 +27,6 @@
                 <?php endif; ?>
 
                 <form action="../controllers/user-controller.php?action=login" method="POST">
-                    <!-- Username field added -->
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" required>
-
                     <!-- Email field added -->
                     <label for="email">Email:</label>
                     <input type="email" id="email" name="email" required>
